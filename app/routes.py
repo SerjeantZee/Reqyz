5232ba746a033b5cecc52e4c8395a32a1935d0f1
Zee (WorkVS) <zeehateemail@yandex.ru>
2024-11-18 18:09:41 +1100

    Less2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,34 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/imdex')
 def index():
-    return 'Hello!'
+    user = {'username': 'Admin'}
+    posts = [
+        {
+            'id': 1,
+            'author': {'username': 'Zee'},
+            'body': 'Шутки про говно не смешные (скорее всего)'
+        },
+        {
+            'id': 2,
+            'author': {'username': 'Саня'},
+            'body': 'Ты нахуя меня сюда добавил?)'
+        }
+    ]
+    comments = [
+        {
+            'id': 1,
+            'post_id': 1,
+            'author': {'username': 'Саня'},
+            'body': 'Ты заебал. Убери меня отсюда.'
+        }
+    ]
+    return render_template(
+        'index.html', 
+        title = 'Домашняя страница', 
+        user = user, 
+        posts = posts,
+        comments = comments
+        )
