c38992fda6b35cb04de137fd62c463670066b579
Zee (WorkVS) <zeehateemail@yandex.ru>
2024-11-19 12:17:46 +1100

    Lesson 3 (closed)

--- app/routes.py
+++ app/routes.py
@@ -34,14 +34,13 @@ def index():
         comments = comments
         )
 
-@app.route('/login', method=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for User {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template(
         'login.html', 
-        title = 'Вход', 
-        form = form
-        )
+        title='Sign In', 
+        form=form)
